f6f58209a6d271263279ad9c4c52c472022e82be
Rus Hughes <russell.hughes@gmail.com>
2018-02-15 18:38:11 +0700

    cleaning up

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var ProvablyRare = artifacts.require("./ProvablyRare.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
